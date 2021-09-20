fa8d28ecb412e5a99a9b73c5226f788165a4cb6a
Александр Ковалев <sashakovalev2002@hotmail.com>
2021-09-20 09:33:30 +0300

    LAB2 and LAB3 are finished

---
+++ Lab2/OS_0001/OS_0001/OS_0001.c
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main() {
+	for (int i = 0; i < 1000; i++)
+	{
+		sleep(2);
+		printf("PID: %d-#%d\n", getpid(), i);
+	}
+	exit(0);
+}
